c2c20f9da18bc324a51110eaabcc40f8e4e70c3c
Pheru <philipp.bruckner@freenet.de>
2018-09-16 01:01:49 +0200

    #45 - Lokale Fehler-Konfig entfernt

--- pheru-darts-react-client/src/services/configService.js
+++ pheru-darts-react-client/src/services/configService.js
@@ -9,27 +9,11 @@ const LOCAL = {
     logoutUrl : "http://localhost:8080/logout",
     signUpUrl : "http://localhost:8080/sign-up"
 };
-const LOCAL_ERROR = {
-    resourceUrls: {
-        users: "http://localhost:8080/testfehler",
-        playerPermission: "http://localhost:8080/testfehler",
-        games: "http://localhost:8080/testfehler",
-        statistics: "http://localhost:8080/testfehler"
-    },
-    loginUrl : "http://localhost:8080/testfehler",
-    logoutUrl : "http://localhost:8080/testfehler",
-    signUpUrl : "http://localhost:8080/testfehler"
-};
 const PRODUCTION = LOCAL;
-
-const LOCAL_ERROR_MODE_STRING = "testmodefail";
 
 export function getConfig() {
     switch (window.location.hostname) {
         case "localhost":
-            if (window.location.href.includes(LOCAL_ERROR_MODE_STRING)) {
-                return LOCAL_ERROR;
-            }
             return LOCAL;
         default:
             return PRODUCTION;
