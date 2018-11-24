02a4ff57aea040e006dbb332ed3dd2dc5fabedfe
Pheru <philipp.bruckner@freenet.de>
2018-11-24 23:47:27 +0100

    S: Logik für User-Änderung entfernt (wird eigene Komponente)

--- pheru-darts-react-client/src/components/Settings.js
+++ pheru-darts-react-client/src/components/Settings.js
@@ -9,20 +9,11 @@ class Settings extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: props.userName !== undefined ? props.userName : "",
-            currentPassword: "",
-            newPassword: "",
-            passwordRepeat: "",
-            passwordsMatch: true,
             userNameToPermit: "",
             permitButtonDisabled: true,
             playersInformation: Settings.mergeUsers(this.props.playableUsers, this.props.permittedUsers)
         };
-        this.handleNameChange = this.handleNameChange.bind(this);
         this.handleUserNameToPermitChange = this.handleUserNameToPermitChange.bind(this);
-        this.handleCurrentPasswordChange = this.handleCurrentPasswordChange.bind(this);
-        this.handleNewPasswordChange = this.handleNewPasswordChange.bind(this);
-        this.handlePasswordRepeatChange = this.handlePasswordRepeatChange.bind(this);
     }
 
     componentDidUpdate(prevProps) {
@@ -66,30 +57,12 @@ class Settings extends React.Component {
         return playerInformation;
     }
 
-    handleNameChange(value) {
-        this.setState({name: value});
-    }
-
     handleUserNameToPermitChange(value) {
         let buttonDisabled = value === undefined || value === null || value === "";
         this.setState({
             userNameToPermit: value,
             permitButtonDisabled: buttonDisabled
         });
-    }
-
-    handleNewPasswordChange(value) {
-        let match = value === this.state.passwordRepeat;
-        this.setState({newPassword: value, passwordsMatch: match});
-    }
-
-    handleCurrentPasswordChange(value) {
-        this.setState({currentPassword: value});
-    }
-
-    handlePasswordRepeatChange(value) {
-        let match = value === this.state.password;
-        this.setState({passwordRepeat: value, passwordsMatch: match});
     }
 
     render() {
@@ -109,44 +82,6 @@ class Settings extends React.Component {
 
     createUserSettings() {
         return <div style={{textAlign: 'center'}}>
-            {/*<div style={{marginBottom: 50}}>*/}
-            {/*<h3 style={{marginLeft: 10, marginRight: 10}}><strong>Benutzer-Einstellungen</strong></h3>*/}
-            {/*<Alert bsStyle="warning" style={{marginLeft: 15, marginRight: 15, marginBottom: 5}}>*/}
-            {/*<Glyphicon glyph="exclamation-sign"/> <strong>Ändern von Benutzer-Einstellungen noch nicht*/}
-            {/*möglich</strong>*/}
-            {/*</Alert>*/}
-            {/*<div style={{marginLeft: 20, marginRight: 20}}>*/}
-            {/*<strong>Benutzername ändern: </strong>*/}
-            {/*<FormControl style={{marginBottom: 5, width: 300}} type="text"*/}
-            {/*value={this.state.name}*/}
-            {/*onChange={(e) => this.handleNameChange(e.target.value)}*/}
-            {/*placeholder="Benutzername"/>*/}
-            {/*<strong>Passwort ändern: </strong>*/}
-            {/*<FormControl style={{marginBottom: 5, width: 300}} type="password"*/}
-            {/*value={this.state.currentPassword}*/}
-            {/*onChange={(e) => this.handleCurrentPasswordChange(e.target.value)}*/}
-            {/*placeholder="Aktuelles Passwort"/>*/}
-            {/*<FormControl style={{marginBottom: 5, width: 300}} type="password"*/}
-            {/*value={this.state.newPassword}*/}
-            {/*onChange={(e) => this.handleNewPasswordChange(e.target.value)}*/}
-            {/*placeholder="Neues Passwort"/>*/}
-            {/*<FormControl style={{marginBottom: 5, width: 300}} type="password"*/}
-            {/*value={this.state.passwordRepeat}*/}
-            {/*onChange={(e) => this.handlePasswordRepeatChange(e.target.value)}*/}
-            {/*placeholder="Neues Passwort wiederholen"/>*/}
-            {/*<Button bsStyle="primary" disabled={true}>Änderungen speichern</Button>*/}
-            {/*<p/><strong>Benutzer löschen: </strong><p/>*/}
-            {/*/!*TODO in Alert auslagern (Hinweis (inklusive Löschen von Statistiken) und PW-Bestätigung)*!/*/}
-            {/*<div style={{color: "#d9534f"}}>*/}
-            {/*<Glyphicon glyph="alert"/> Dieser Vorgang kann nicht rückgängig gemacht werden <Glyphicon*/}
-            {/*glyph="alert"/>*/}
-            {/*</div>*/}
-            {/*<FormControl style={{marginBottom: 5, width: 300}} type="password"*/}
-            {/*// value={this.state.password}*/}
-            {/*// onChange={(e) => this.handlePasswordChange(e.target.value)}*/}
-            {/*placeholder="Passwort bestätigen"/>*/}
-            {/*<Button bsStyle="danger" disabled={true}>Benutzer löschen</Button>*/}
-            {/*</div>*/}
             <h3 style={{marginTop: 0}}><strong>Spieler-Berechtigungen</strong></h3>
             <div>
                 <p><strong>Hier siehst Du welche Spieler Dir erlaubt haben ein Spiel mit ihnen zu erstellen und
