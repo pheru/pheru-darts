7c9decc506044151008f8b5403cf9003ae74305c
Pheru <philipp.bruckner@freenet.de>
2019-02-03 02:08:17 +0100

    1.2.0

---
+++ precache-manifest.0e4df127c342f1b46a3e551017f892b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "ec6a8eb84d5faab3e992",
+    "url": "./static/js/main.ec6a8eb8.chunk.js"
+  },
+  {
+    "revision": "f8d4d39dbd0205646254",
+    "url": "./static/js/1.f8d4d39d.chunk.js"
+  },
+  {
+    "revision": "ec6a8eb84d5faab3e992",
+    "url": "./static/css/main.f096a765.chunk.css"
+  },
+  {
+    "revision": "9012ccc92a0827a44d2eb87b408f1688",
+    "url": "./index.html"
+  }
+];
